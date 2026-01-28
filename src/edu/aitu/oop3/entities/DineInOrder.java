b438e853e704760212dee5bcff36cfdb108628c7
Azamat Ray <250221@astanait.edu.kz>
2026-01-28 23:25:21 +0500

    Save my current changes

--- src/edu/aitu/oop3/entities/DineInOrder.java
+++
@@ -1,8 +0,0 @@
-package edu.aitu.oop3.entities;
-import java.time.OffsetDateTime;
-public class DineInOrder extends BaseOrder {
-    public DineInOrder(long customerId) {
-        super(0, customerId, OrderStatus.ACTIVE, OffsetDateTime.now());
-    }
-}
-
